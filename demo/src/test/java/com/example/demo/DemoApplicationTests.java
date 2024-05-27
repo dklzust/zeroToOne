c58995900f4ec15a819bb2facc011b01356fad04
dongkl <dongkl@sucsoft.com>
2024-05-27 13:50:49 +0800

    chore:配置redis环境

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class DemoApplicationTests {
-
+	@Autowired
+	private RedisTemplate<String, Object> redisTemplate;
 	@Test
 	void contextLoads() {
+		redisTemplate.opsForValue().set("dkl","123");
 	}
 
 }
